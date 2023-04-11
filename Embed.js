dc46d32e7b5f9493137fc02844400afe7618b21b
rwrftr <104785633+rwrftr@users.noreply.github.com>
2023-04-11 14:59:46 -0700

    Update Embed.js

--- Embed.js
+++ Embed.js
@@ -1,14 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 function embedFunc(url) \{\
-  const t1 = '<div style="position:relative;padding-top:max(60%,326px);height:0;width:100%"><iframe allow="clipboard-write" sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox allow-forms" allowfullscreen="true" style="position:absolute;border:none;width:100%;height:100%;left:0;right:0;top:0;bottom:0;" src="https://e.issuu.com/embed.html?d=';\
-  const t2 = url.substring(32);\
-  const t3 = '&u=thelowell"></iframe></div>';\
-  return t1 + t2 + t3;\
-\}\
+function embedFunc(url) {
+  const t1 = '<div style="position:relative;padding-top:max(60%,326px);height:0;width:100%"><iframe allow="clipboard-write" sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox allow-forms" allowfullscreen="true" style="position:absolute;border:none;width:100%;height:100%;left:0;right:0;top:0;bottom:0;" src="https://e.issuu.com/embed.html?d=';
+  const t2 = url.substring(32);
+  const t3 = '&u=thelowell"></iframe></div>';
+  return t1 + t2 + t3;
 }
